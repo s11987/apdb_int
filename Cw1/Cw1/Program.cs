42f4a83bff4e3556c2d501c5ab3d922196992e6f
Karol Gałązka <s11987@pjwstk.edu.pl>
2020-04-10 11:12:35 +0200

    cw1_zd3

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            string url = Console.ReadLine();
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+
+                var matches = regex.Matches(html);
+                foreach(var item in matches)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
+            Console.WriteLine("Koniec");
         }
     }
 }
